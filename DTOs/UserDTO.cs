b668d4723f9e75af3621ef433801ce2f05ea1e08
Tô Nguyễn Khánh Nguyên <nguyentnkse183375@fpt.edu.vn>
2026-02-23 23:22:29 +0700

    CRUD Admin

---
+++ DTOs/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿public class UserDTO
+{
+    public int UserID { get; set; }
+    public string UserName { get; set; }
+    public string FullName { get; set; }
+    public string Role { get; set; }
+    public string Email { get; set; }
+    public bool IsActive { get; set; }
+}
---
+++ Models/User.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YourProject.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string Expertise { get; set; }
+
+        public int Score { get; set; } = 100;
+
+        public int CurrentTaskCount { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public int ConsecutiveFailCount { get; set; }
+
+        public int ConsecutiveReviewCount { get; set; }
+
+        public int SpamCount { get; set; }
+
+        public int TotalTaskCompleted { get; set; }
+    }
+}
